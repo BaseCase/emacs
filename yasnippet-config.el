72b828805fee6bac5d1855958bc43375f42c07d9
Casey Brant <casey.brant@gmail.com>
2012-02-08 20:21:44 -0800

    Fix YAS initializing.

--- yasnippet-config.el
+++ yasnippet-config.el
@@ -1,7 +1,8 @@
 (add-to-list 'load-path (concat cjb-load-path "yasnippet"))
 (require 'yasnippet)
+
+(setq yas/root-directory (concat cjb-load-path "yasnippet/snippets"))
+(yas/load-directory yas/root-directory)
 (yas/initialize)
-(yas/load-directory (concat cjb-load-path "yasnippet/snippets"))
-(yas/global-mode 1)
 
 (provide 'yasnippet-config)
