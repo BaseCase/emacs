bc0b409b0f69a5954affea1aadf04b3f3481e8e4
Casey Brant <casey.brant@gmail.com>
2012-02-09 10:18:06 -0800

    Always turn on the pretty indenting for org-mode.

--- org-config.el
+++ org-config.el
@@ -1,6 +1,7 @@
 (setq org-hide-leading-stars t)
 (setq org-directory "~/Dropbox/org")
 (setq org-default-notes-file (concat org-directory "/capture.org"))
+(setq org-startup-indented t)
 
 (setq org-capture-templates
       '(("r" "remember" entry (file "")
