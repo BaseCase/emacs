4236e874866a3c72aa406b0ff5daf97dbe07c6cc
Casey Brant <casey.brant@gmail.com>
2012-02-09 13:16:59 -0800

    Visualize TAB characters (but not other crap).

--- whitespace-config.el
+++ whitespace-config.el
@@ -1,4 +1,14 @@
 (setq-default tab-width 4)
+(global-whitespace-mode t)
+(setq whitespace-style
+ '(face
+   space-before-tab
+   tab-mark))
 
+;;Smart Tabs Mode: doesn't seem to be working, but could be useful later.
+;(add-to-list 'load-path (concat cjb-load-path "smarttabs"))
+;(require 'smart-tabs-mode)
+;(add-hook 'js2-mode-hook 'smart-tabs-mode-enable)
+;(smart-tabs-advice js2-indent-line js2-basic-offset)
 
 (provide 'whitespace-config)
