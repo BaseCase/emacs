405a7459099f796b8d98d7d57f1ceaf235a45ddb
Casey Brant <casey.brant@gmail.com>
2012-03-05 18:10:53 -0800

    markdown-mode wasn't working, so now we're requiring it.

--- markdown-config.el
+++ markdown-config.el
@@ -1,4 +1,6 @@
 (add-to-list 'load-path (concat cjb-load-path "markdown-mode"))
+(require 'markdown-mode)
+
 (autoload 'markdown-mode "markdown-mode.el" t)
 (setq auto-mode-alist (cons '("\\.text" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
