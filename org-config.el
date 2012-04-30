63a54874fbd89f9e35aa5dc58e8e8aa7a6ba254c
Casey Brant <casey.brant@gmail.com>
2012-04-30 16:21:19 -0500

    Rebind window resizing keys in Org mode (they were getting shadowed)

--- org-config.el
+++ org-config.el
@@ -12,5 +12,15 @@
           '(lambda ()
              (kill-buffer "capture.org")))
 
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (set-cjb-org-keys)))
+
+(defun set-cjb-org-keys ()
+  (local-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+  (local-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+  (local-set-key (kbd "S-C-<down>") 'shrink-window)
+  (local-set-key (kbd "S-C-<up>") 'enlarge-window))
+
 
 (provide 'org-config)
