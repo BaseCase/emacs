8f81d222ed73ff9a90f86326c47abefd6434fdcf
Casey Brant <casey.brant@gmail.com>
2012-10-08 11:40:23 -0700

    Auto-resize on window switch! (except it doesn't count columns right yet)

--- visuals-config.el
+++ visuals-config.el
@@ -8,6 +8,12 @@
 (set-face-background 'hl-line "#333")
 (setq mouse-wheel-scroll-amount '(1))
 (setq mouse-wheel-progressive-speed nil)
+(add-to-list 'load-path (concat cjb-load-path "rainbow-mode"))
+(require 'rainbow-mode)
+
+;;vvv neither of these make window size the right thing vvv
+;;(set-face-attribute 'default nil :font "-apple-Ubuntu_Mono-medium-normal-normal-*-17-*-*-*-p-0-iso10646-1")
+;;(set-default-font "-apple-Ubuntu_Mono-medium-normal-normal-*-17-*-*-*-p-0-iso10646-1")
 
 (setq default-frame-alist
       '(
@@ -26,7 +32,14 @@
      ;(color-theme-hober)
      ))
 
-(add-to-list 'load-path (concat cjb-load-path "rainbow-mode"))
-(require 'rainbow-mode)
+(defadvice other-window (after dynamic-resize-on-switch-window activate)
+  "Switching windows causes the new active to have at least 80 columns"
+  (when (< (window-body-width) 80)
+    (enlarge-window (- 80 (window-body-width)) t)))
+
+(defun get-window-size ()
+  "Temporary utility function to help debug window resizing problems"
+  (interactive)
+  (message "The width is %d." (window-body-width)))
 
 (provide 'visuals-config)
