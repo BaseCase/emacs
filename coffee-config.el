5cb8343eee8935e93d19c5e39a2c0c8dfbb47bd8
Casey Brant <casey.brant@gmail.com>
2012-10-25 09:47:13 -0700

    Auto-load html-mode for Backbone template files (instead of coffee-mode)

--- coffee-config.el
+++ coffee-config.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path (concat cjb-load-path "coffee-mode"))
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("_template\\.coffee$" . html-mode)) ;; for Backbone template files
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
 (provide 'coffee-config)
