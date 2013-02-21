129364cccee1e625f3012bd1c1e13894e7802c94
Casey Brant <casey.brant@gmail.com>
2013-02-21 12:32:32 -0800

    By default, don't show JS parse errors.

--- javascript-config.el
+++ javascript-config.el
@@ -4,6 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook '(lambda ()
                             (setq js2-bounce-indent-p t)
-                            (setq js2-basic-offset 2)))
+                            (setq js2-basic-offset 2)
+                            (js2-mode-hide-warnings-and-errors)))
 
 (provide 'javascript-config)
