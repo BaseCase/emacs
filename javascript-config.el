4b7605dd33e8f5d5994915fb2789ccee9bd7732c
Casey Brant <casey.brant@gmail.com>
2013-02-21 12:29:51 -0800

    Trying out 2-space indents for JS files...see how it goes

--- javascript-config.el
+++ javascript-config.el
@@ -2,7 +2,8 @@
 (require 'js2-mode)
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(setq js2-bounce-indent-p t)
-(setq js2-enter-indents-newline t)
+(add-hook 'js2-mode-hook '(lambda ()
+                            (setq js2-bounce-indent-p t)
+                            (setq js2-basic-offset 2)))
 
 (provide 'javascript-config)
