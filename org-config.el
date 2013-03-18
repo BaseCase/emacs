32d009dbc774c85a93a4def95411f1f1cf95450a
Casey Brant <casey.brant@gmail.com>
2013-03-17 19:00:33 -0700

    Clean up org-config a little bit

--- org-config.el
+++ org-config.el
@@ -1,13 +1,6 @@
 (setq org-hide-leading-stars t)
 (setq org-startup-indented t)
 
-(defun cjb-back-to-indentation-org-version ()
-  "Like M-m but skips the line item decorators in org mode"
-  (interactive "^")
-  (beginning-of-line 1)
-  (skip-chars-forward "*-")
-  (forward-char))
-
 (add-hook 'org-mode-hook '(lambda () (cjb-org-mode-keys)))
 
 (defun cjb-org-mode-keys ()
@@ -16,8 +9,14 @@
   (local-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
   (local-set-key (kbd "S-C-<down>") 'shrink-window)
   (local-set-key (kbd "S-C-<up>") 'enlarge-window)
-  (local-set-key (kbd "C-j") 'org-insert-todo-heading)
   (local-set-key (kbd "C-'") 'ido-switch-buffer)
   (local-set-key (kbd "M-m") 'cjb-back-to-indentation-org-version))
+
+(defun cjb-back-to-indentation-org-version ()
+  "Like M-m but skips the line item decorators in org mode"
+  (interactive "^")
+  (beginning-of-line 1)
+  (skip-chars-forward "*-")
+  (forward-char))
 
 (provide 'org-config)
