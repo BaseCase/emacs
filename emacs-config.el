53634efe3a3c9bc9c4a985239a59d811c30172b6
Casey Brant <casey.brant@gmail.com>
2013-03-17 20:30:34 -0700

    clean up emacs-config.el

--- emacs-config.el
+++ emacs-config.el
@@ -1,31 +1,29 @@
 (defvar cjb-load-path "~/emacs/lib/")
 
-(setq make-backup-files nil)
-(put 'narrow-to-region 'disabled nil)
+;; random config not attached to a specific mode
+(setq make-backup-files nil
+      ido-enable-flex-matching t
+      help-window-select t
+      ispell-program-name "/usr/local/bin/aspell")
 (ido-mode t)
-(set-variable 'ido-enable-flex-matching t)
-(set-variable 'help-window-select t)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq ispell-program-name "/usr/local/bin/aspell")
-(server-start)
+(add-hook 'grep-mode-hook 'toggle-truncate-lines)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(defalias 'grep-find-dired 'find-grep-dired)
 
-;;Dired buffer names always end in a '/'
+;; make Unicode work right
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; dired buffer names always end in a '/'
 (add-hook 'dired-mode-hook 'ensure-buffer-name-ends-in-slash)
 (defun ensure-buffer-name-ends-in-slash ()
   "change buffer name to end with slash"
   (let ((name (buffer-name)))
     (if (not (string-match "/$" name))
         (rename-buffer (concat name "/") t))))
-
-(add-hook 'grep-mode-hook 'toggle-truncate-lines)
-(defalias 'grep-find-dired 'find-grep-dired)
-
-;;unicode fixing
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
 
 (defun path ()
   "show current buffer's full filename and copy it to clipboard"
@@ -40,14 +38,16 @@
                 "/usr/sbin:"
                 "/usr/local/bin:"))
 
+(server-start)
+
+(require 'coffee-config)
+(require 'command-runner)
+(require 'javascript-config)
+(require 'keys-config)
+(require 'markdown-config)
 (require 'org-config)
 (require 'visuals-config)
-(require 'keys-config)
-(require 'yasnippet-config)
 (require 'whitespace-config)
-(require 'javascript-config)
-(require 'coffee-config)
-(require 'markdown-config)
-(require 'command-runner)
+(require 'yasnippet-config)
 
 (provide 'emacs-config)
