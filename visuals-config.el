240a7ab4c68b5e92c20d2e55ca263459345ad7d8
Casey Brant <casey.brant@gmail.com>
2013-03-17 22:40:41 -0700

    Port vividchalk to deftheme style and kill color-theme [fixes #77]

--- visuals-config.el
+++ visuals-config.el
@@ -1,6 +1,5 @@
-(add-to-list 'load-path (concat cjb-load-path "color-theme"))
-(require 'color-theme)
-
+(add-to-list 'custom-theme-load-path "~/emacs")
+(load-theme 'vivid-chalk t)
 (setq inhibit-splash-screen t
       initial-scratch-message nil
       scroll-conservatively 5)
@@ -8,18 +7,11 @@
 (show-paren-mode 1)
 (column-number-mode 1)
 (global-hl-line-mode t)
-(set-face-background 'hl-line "#333")
 
 (setq default-frame-alist
       '(
-        (font . "-apple-Ubuntu_Mono-medium-normal-normal-*-18-*-*-*-m-0-iso10646-1")
         (width . 80)
         (height . 30)))
-
-(eval-after-load "color-theme"
-  '(progn
-     (color-theme-initialize)
-     (color-theme-vivid-chalk)))
 
 (defadvice other-window (after dynamic-resize-on-switch-window activate)
   "Switching windows causes the new active to have at least 80 columns"
