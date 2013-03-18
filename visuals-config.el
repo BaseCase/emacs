fb2cc54c65bcd0f352972e3fb3df4fbcfd7e0086
Casey Brant <casey.brant@gmail.com>
2013-03-17 17:12:32 -0700

    Clean out a couple visuals settings that were unused.

--- visuals-config.el
+++ visuals-config.el
@@ -9,9 +9,6 @@
 (column-number-mode 1)
 (global-hl-line-mode t)
 (set-face-background 'hl-line "#333")
-(setq mouse-wheel-scroll-amount '(1))
-(setq mouse-wheel-progressive-speed nil)
-
 
 (setq default-frame-alist
       '(
@@ -22,11 +19,7 @@
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-vivid-chalk)
-     ;(color-theme-vibrant-ink)
-     ;(color-theme-zenburn)
-     ;(color-theme-hober)
-     ))
+     (color-theme-vivid-chalk)))
 
 (defadvice other-window (after dynamic-resize-on-switch-window activate)
   "Switching windows causes the new active to have at least 80 columns"
