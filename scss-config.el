aef3cf8d754501e514079a1b5aeec81e9da7a64a
Casey Brant <casey.brant@gmail.com>
2013-03-17 18:51:44 -0700

    Remove SCSS stuff

--- scss-config.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'load-path (concat cjb-load-path "scss-mode"))
-(require 'scss-mode)
-
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-(set-variable 'scss-compile-at-save nil)
-
-(provide 'scss-config)
