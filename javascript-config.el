e7c5bf821e32979490613ef3d1e8ba3d62e7b22c
Casey Brant <casey.brant@gmail.com>
2013-03-19 13:57:07 -0700

    whitespace rules for current job house style

--- javascript-config.el
+++ javascript-config.el
@@ -4,7 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook '(lambda ()
                             (setq js2-bounce-indent-p t)
-                            (setq js2-basic-offset 2)
+                            (setq js2-basic-offset 4) ;; normally prefer 2, this is job's house style
                             (js2-mode-hide-warnings-and-errors)))
 
 (provide 'javascript-config)
--- whitespace-config.el
+++ whitespace-config.el
@@ -10,4 +10,8 @@
         trailing))
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;; current job house style:
+(add-hook 'html-mode-hook '(lambda ()
+                             (set (make-local-variable 'sgml-basic-offset) 4)))
+
 (provide 'whitespace-config)
