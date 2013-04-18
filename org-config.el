104473941d1f81abbe4b2d6c1095138bfaa7a3a3
Casey Brant <casey.brant@gmail.com>
2013-04-17 18:58:06 -0700

    whitespace alignment in org keys config

--- org-config.el
+++ org-config.el
@@ -14,14 +14,14 @@
 
 (defun cjb-org-mode-keys ()
   "set keys I like in org buffers"
-  (local-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+  (local-set-key (kbd "S-C-<left>")  'shrink-window-horizontally)
   (local-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-  (local-set-key (kbd "S-C-<down>") 'shrink-window)
-  (local-set-key (kbd "S-C-<up>") 'enlarge-window)
-  (local-set-key (kbd "C-'") 'ido-switch-buffer)
-  (local-set-key (kbd "M-m") 'cjb-back-to-indentation-org-version)
-  (local-set-key (kbd "M-h")  'ns-do-hide-emacs)
-  (local-set-key (kbd "C-c C-.")  'org-time-stamp))
+  (local-set-key (kbd "S-C-<down>")  'shrink-window)
+  (local-set-key (kbd "S-C-<up>")    'enlarge-window)
+  (local-set-key (kbd "C-'")         'ido-switch-buffer)
+  (local-set-key (kbd "M-m")         'cjb-back-to-indentation-org-version)
+  (local-set-key (kbd "M-h")         'ns-do-hide-emacs)
+  (local-set-key (kbd "C-c C-.")     'org-time-stamp))
 
 (defun cjb-back-to-indentation-org-version ()
   "Like M-m but skips the line item decorators in org mode"
