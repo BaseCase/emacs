53ac0ff5d17cc3f218b159580c8125c3664f5582
Casey Brant <casey.brant@gmail.com>
2013-05-01 10:32:01 -0700

    reorganize whitespace config, keep all tab settings in whitespace.el

--- javascript-config.el
+++ javascript-config.el
@@ -4,7 +4,6 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook '(lambda ()
                             (setq js2-bounce-indent-p t)
-                            (setq js2-basic-offset 4) ;; normally prefer 2, this is job's house style
                             (js2-mode-hide-warnings-and-errors)))
 
 (provide 'javascript-config)
--- whitespace-config.el
+++ whitespace-config.el
@@ -1,20 +1,21 @@
-(setq-default tab-width 4)
-(setq tab-stop-list (number-sequence 4 120 4))
-(global-whitespace-mode t)
 (setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+
+(setq css-indent-offset 2)
+(setq python-indent-offset 2)
+(setq sgml-basic-offset 2)
+(custom-set-variables
+ '(js2-basic-offset 2))
+
+(global-whitespace-mode t)
+(add-hook 'before-save-hook 'whitespace-cleanup)
 (setq whitespace-style
       '(face
         tabs
         tab-mark
         space-before-tab
         trailing))
+
 (setq sentence-end-double-space nil)
-
-;;disable for now...getting too many whitespace diffs in current codebase
-;;(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; current job house style:
-(add-hook 'html-mode-hook '(lambda ()
-                             (set (make-local-variable 'sgml-basic-offset) 4)))
 
 (provide 'whitespace-config)
