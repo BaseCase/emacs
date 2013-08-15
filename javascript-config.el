570054f7e91d13d6eb56b4c54519117f41e9935b
Casey Brant <casey.brant@gmail.com>
2013-08-15 16:51:29 -0500

    turn off js2-mode indent bounce cuz it's horrible

--- javascript-config.el
+++ javascript-config.el
@@ -3,7 +3,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook '(lambda ()
-                            (setq js2-bounce-indent-p t)
+                            (setq js2-bounce-indent-p nil)
                             (js2-mode-hide-warnings-and-errors)))
 
 (provide 'javascript-config)
