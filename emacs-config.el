2a437a32479d87a391f615fca5c884817ab49ad4
Casey Brant <casey.brant@gmail.com>
2014-09-11 17:00:20 -0700

    mac os shortcut keys

--- emacs-config.el
+++ emacs-config.el
@@ -1,2 +1,9 @@
 (defvar cjb-load-path "~/emacs/lib/")
+
+;; on OS X, set modifier keys nicely
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
 (provide 'emacs-config)
